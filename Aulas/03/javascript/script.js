f462802c90528a841d174595764b942b6392eee2
João Pedro Miranda Santos <jpmsantosti@gmail.com>
2026-03-06 08:45:07 -0300

    Add files via upload

---
+++ Aulas/03/javascript/script.js
@@ -0,0 +1,8 @@
+function soma()
+{
+    nome = document.getElementById("username").value;
+    cpf = document.getElementById("cpf").value;
+    endereco = document.getElementById("endereco").value;
+    email = document.getElementById("email").value;
+    alert("olá "+ nome + " seu CPF é "+ cpf+ " o seu endereço é "+endereco +" e o seu email é "+email);
+}
